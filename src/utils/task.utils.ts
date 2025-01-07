1b3f32799c80e424ffa15173a612003c03397bc8
uladzimir <stankevichuuladimiru@gmail.com>
2025-01-08 00:33:42 +0300

    fix: change tasks type

--- src/utils/task.utils.ts
+++ src/utils/task.utils.ts
@@ -1,6 +1,8 @@
 import tasks from '../assets/data/tasks.json'
-const getTasksByType = (type: string) => {
-    return tasks.filter((task) => task.type === type)
+import { TaskItem, TaskType } from '../models/task.model'
+
+const getTasksByType = (type: TaskType) => {
+    return tasks.filter((task) => task.type === type) as TaskItem[]
 }
 
 export { getTasksByType }
