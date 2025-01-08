99f96cea315901d5d4624c0d786c9b14fb9d37e3
uladzimir <stankevichuuladimiru@gmail.com>
2025-01-08 23:12:12 +0300

    refactor: change timestamp-to-date-convertor util logic

--- src/utils/common.utils.ts
+++ src/utils/common.utils.ts
@@ -1,10 +1,4 @@
-const options: Intl.DateTimeFormatOptions = {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-}
-
 const timestampToDateConvertor = (timestamp: number | string) =>
-    new Date(timestamp).toLocaleString('en-En', options)
+    new Date(timestamp).toLocaleDateString('ru-RU')
 
 export { timestampToDateConvertor }
