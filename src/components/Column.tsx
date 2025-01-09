e714fc669be535ef3e701131793f2718f8d3317f
uladzimir <stankevichuuladimiru@gmail.com>
2025-01-09 21:50:27 +0300

    feat: add column-header component to app

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,9 +1,10 @@
 import clsx from 'clsx'
 
-import { ColumnNameType, TaskItem, TaskType } from '../models/task.model'
+import { TaskItem, TaskType } from '../models/task.model'
 import { getTasksByType } from '../utils/task.utils'
 
 import TaskCard from './TaskCard'
+import ColumnHeader from './ColumnHeader'
 
 interface ColumnProps {
     taskType: TaskType
@@ -15,9 +16,7 @@ function Column({ taskType, className }: ColumnProps) {
         <article
             className={clsx('py-8 px-4 bg-columnBackground rounded', className)}
         >
-            <h2 className="text-foregroundBold text-2xl font-bold mb-4">
-                {ColumnNameType[taskType]}
-            </h2>
+            <ColumnHeader taskType={taskType} />
             <ul className={'flex flex-col gap-4'}>
                 {tasks.map((task) => (
                     <li key={task.id}>
