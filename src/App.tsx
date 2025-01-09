20bf4e8892987eca7c79a25d6c5e1611077a6a13
uladzimir <stankevichuuladimiru@gmail.com>
2025-01-09 11:06:29 +0300

    feat: add app title

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import Header from './components/Header'
+// import Header from './components/Header'
 import Board from './components/Board'
 
 const App = () => {
     return (
         <>
-            <Header />
-            <main className="flex flex-row justify-center">
+            {/* <Header /> */}
+            <main className="flex flex-col items-center">
+                <div className="w-full max-w-screen-fullHd mt-16 mb-[58px]">
+                    <h1 className="max-w-screen-fullHd text-foregroundBold text-[40px] font-bold">
+                        Your tasks
+                    </h1>
+                </div>
                 <Board />
             </main>
         </>
