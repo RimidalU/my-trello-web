f6ef0e9ddb13226768cb2744c5db143f6fda9ade
uladzimir <stankevichuuladimiru@gmail.com>
2025-01-11 23:39:53 +0300

    feat: add load-tasks action type

--- src/models/taskContext.model.ts
+++ src/models/taskContext.model.ts
@@ -4,12 +4,14 @@ enum TaskAction {
     add_task = 'ADD_TASK',
     remove_task = 'REMOVE_TASK',
     update_task = 'UPDATE_TASK',
+    load_tasks = 'LOAD_TASKS',
 }
 
 type Action =
     | { type: TaskAction.add_task; task: TaskItem }
     | { type: TaskAction.remove_task; id: number }
     | { type: TaskAction.update_task; task: TaskItem }
+    | { type: TaskAction.load_tasks; tasks: TaskItem[] }
 
 type State = {
     tasks: TaskItem[]
