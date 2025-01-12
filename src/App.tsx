9d77e6d6b095c6b81461c0e57c94989021ee4dee
uladzimir <stankevichuuladimiru@gmail.com>
2025-01-12 22:21:06 +0300

    fix: implement full screen drop items

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-// import Header from './components/Header'
 import Board from './components/Board'
 import { TaskProvider } from './contexts/TaskProvider'
 
 const App = () => {
     return (
         <>
-            {/* <Header /> */}
-            <main className="flex flex-col items-center">
+            <main className="flex h-full flex-col items-center overflow-hidden pb-10">
                 <div className="w-full max-w-screen-fullHd mt-16 mb-[58px]">
                     <h1 className="max-w-screen-fullHd text-foregroundBold text-[40px] font-bold">
                         Your tasks
