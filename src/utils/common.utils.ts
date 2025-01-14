baa2af21d63120b00fcb6308de216244a5062d75
uladzimir <stankevichuuladimiru@gmail.com>
2025-01-14 09:36:49 +0300

    feat: implement get-start-and-end-of-day function

--- src/utils/common.utils.ts
+++ src/utils/common.utils.ts
@@ -1,4 +1,14 @@
 const timestampToDateConvertor = (timestamp: number | string) =>
     new Date(timestamp).toLocaleDateString('ru-RU')
 
-export { timestampToDateConvertor }
+const getStartAndEndOfDay = (date: string) => {
+    const [day, month, year] = date.split('.').map(Number)
+    const timeStamp = new Date(year, month - 1, day)
+
+    const startOfDay = new Date(timeStamp.setHours(0, 0, 0, 0)).getTime()
+    const endOfDay = new Date(timeStamp.setHours(23, 59, 59, 999)).getTime()
+
+    return { startOfDay, endOfDay }
+}
+
+export { timestampToDateConvertor, getStartAndEndOfDay }
