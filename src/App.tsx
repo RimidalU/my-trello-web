9e2c9fce2724103295204e6cdd79639d7d2eaf73
uladzimir <stankevichuuladimiru@gmail.com>
2025-01-14 22:57:12 +0300

    feat: change title position

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,11 @@ import { TaskProvider } from './contexts/TaskProvider'
 const App = () => {
     return (
         <>
-            <main className="flex h-full flex-col items-center  w-fit md:w-full md:overflow-hidden pb-5 xl:pb-10 px-2 xl:px-10 fullHd:px-0">
+            <main className="flex h-screen flex-col items-center w-fit md:w-full md:overflow-hidden pb-5 xl:pb-10 px-2 xl:px-10 fullHd:px-0">
                 <TaskProvider>
-                    <div className="w-full max-w-screen-fullHd mt-16 mb-[58px] flex flex-row justify-between">
-                        <h1 className="text-white-87 text-3xl xl:text-[40px] font-bold">
+                    {/* w-full */}
+                    <div className="self-start md:self-auto md:w-screen max-w-screen-fullHd mt-16 mb-[58px] flex flex-row justify-between">
+                        <h1 className="text-white-87 text-3xl xl:text-[40px] font-bold  m-r-10 md:m-r-0">
                             Your tasks
                         </h1>
                         <TaskFilter />
