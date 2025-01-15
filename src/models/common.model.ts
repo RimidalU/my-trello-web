24967f60189795462eb6f1615bbe2abfbdbf9abb
uladzimir <stankevichuuladimiru@gmail.com>
2025-01-15 10:05:17 +0300

    feat: implement field-name-type

--- src/models/common.model.ts
+++ src/models/common.model.ts
@@ -2,4 +2,10 @@ interface IconComponentProps {
     className?: string
 }
 
+enum FieldNameType {
+    startDay = 'Начало',
+    endDay = 'Окончание',
+    text = 'Описание',
+}
 export type { IconComponentProps }
+export { FieldNameType }
